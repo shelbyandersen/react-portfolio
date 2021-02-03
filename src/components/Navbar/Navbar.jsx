8915899056475a8d9ba1d82232730fcec541b029
Shelby <shelbyrobison14@gmail.com>
2021-02-02 21:02:14 -0500

    brand statement is added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#about">
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <a className="navbar-brand" href="#about">
         Shelby Andersen
       </a>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarNav"
@@ -16,22 +16,22 @@ function Navbar() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#contact">
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">
               Contact
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#portfolio">
+          <li className="nav-item">
+            <a className="nav-link" href="#portfolio">
               Portfolio
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#about">
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
               About
             </a>
           </li>
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Home.css";
 
+
 const Home = () => {
   return (
     <div>
-<p>This is my Home Page</p>
+    <div class="container-fluid watercolor-background">
+      <div class="row">
+        <div class="col-sm-12" id="brand-statement">
+          <h1>Brand Statement</h1>
+          <p class="brand-text">
+            eCommerce Manager specializing in analyzing and implementing
+            strategies that improve overall organization goals. Passionate about
+            efficiency and high-quality work. Strong work ethic, ambition, and
+            desire to learn. Currently attending Georgia Tech's Full-Stack Web
+            Development Program.
+          </p>
+          <button type="button" class="btn btn-light">
+            <a href="./assets/sa-resume.pdf" target="blank">Resume</a>
+          </button>
+        </div>
+      </div>
+    </div>
  
     </div>
   );
